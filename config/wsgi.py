5213485f47686e189fa7445bc2b29885c2f44d29
kutman09 <kutmannurkalykov@gmail.com>
2026-04-02 20:00:27 +0600

    Fixeeeed

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,13 +9,8 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
-
-# SQLite on Vercel is ephemeral (/tmp), so ensure tables exist on cold start.
-if os.getenv("VERCEL"):
-    call_command("migrate", interactive=False, run_syncdb=True, verbosity=0)
