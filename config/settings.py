15b1e9c0f4d977c591569ff2d1016e57a58510fe
kutman09 <kutmannurkalykov@gmail.com>
2026-04-02 18:38:00 +0600

    Исправил некоторые ошибки

--- config/settings.py
+++ config/settings.py
@@ -54,27 +54,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-if os.getenv("DB_NAME"):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("DB_NAME"),
-            "USER": os.getenv("DB_USER", "postgres"),
-            "PASSWORD": os.getenv("DB_PASSWORD", ""),
-            "HOST": os.getenv("DB_HOST", "localhost"),
-            "PORT": os.getenv("DB_PORT", "5432"),
-        }
+# Database (SQLite only for now)
+# On Vercel filesystem is read-only except /tmp.
+SQLITE_DB_PATH = "/tmp/db.sqlite3" if os.getenv("VERCEL") else BASE_DIR / "db.sqlite3"
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": SQLITE_DB_PATH,
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 
 # Password validation
@@ -121,5 +109,6 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 LOGIN_REDIRECT_URL = "shop:product_list"
 LOGOUT_REDIRECT_URL = "shop:product_list"
+LOGIN_URL = "shop:login"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
